fd3b0272ef4fe37a48bb684dd38d2e7f06588796
liulang <809721414@qq.com>
2022-10-18 00:06:31 +0800

    feat: :sparkles: squoosh 命令支持文件夹多层级压缩图片

--- src/bin/squoosh.mjs
+++ src/bin/squoosh.mjs
@@ -1,17 +1,30 @@
-import { $, fs } from 'zx'
+import { $, cd, fs } from 'zx'
 import { join } from 'path'
 
 export async function squoosh (path) {
   const currentPath = path || process.cwd()
-  const dir = fs.readdirSync(currentPath)
   $.verbose = true
+  cd(currentPath)
+  const dir = fs.readdirSync(currentPath)
   if (process.platform === 'win32') $.shell = 'C:\\Program Files\\PowerShell\\7\\pwsh.exe'
   for (const item of dir) {
     const location = join(currentPath, item)
     const info = fs.statSync(location)
-    if (info.isFile()) {
+    if (info.isFile() && ['jpg', 'jpeg', 'png', 'webp'].includes(getExtension(item))) {
       await $`squoosh-cli --avif '{"cqLevel":33,"cqAlphaLevel":-1,"denoiseLevel":0,"tileColsLog2":0,"tileRowsLog2":0,"speed":6,"subsample":1,"chromaDeltaQ":false,"sharpness":0,"tune":0}' -d squoosh ${item}`
+    } else if (info.isDirectory() && item !== 'squoosh') {
+      await squoosh(join(currentPath, item))
     }
   }
   $.verbose = false
+}
+
+/**
+ * 获取文件后缀
+ * @param {string} filename 文件名称
+ * @returns 文件后缀
+ */
+function getExtension (filename) {
+  const ext = (filename || '').split('.')
+  return ext.length > 1 ? ext[ext.length - 1] : ''
 }
